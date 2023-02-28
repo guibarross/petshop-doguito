0a5a02325a62b540ac9dc8df3ffcfe254ee1440f
guibarross <guilhermecbarros7@gmail.com>
2023-02-28 13:52:58 -0300

    validacao de campos do formulario

--- js/app.js
+++ js/app.js
@@ -4,6 +4,6 @@ const inputs = document.querySelectorAll('input')
 
 inputs.forEach(input => {
     input.addEventListener('blur', (evento) => {
-    valida(evento.target)
-})
+        valida(evento.target)
+    })
 })
